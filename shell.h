cd41f82ab04f63221c3515df9a72a67a88e14694
Jeffrey Jiang <jjiang30@stuy.edu>
2022-11-28 22:44:31 -0500

    added function prototypes + comments

---
+++ shell.h
@@ -0,0 +1,14 @@
+#ifndef SHELL_H
+#define SHELL_H
+
+/**
+ * splits the current process and executes the provided command
+ * in the child process. The parent process will wait until the 
+ * child process has finished.
+ * 
+ * @arg command - the command to execute as a string
+ * @return a boolean that signifies if the command was successful or failed
+ */
+char shellexec(char *command);
+
+#endif
