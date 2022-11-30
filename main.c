e9fbd636d597da6f3aae522f3119ec71695a5b42
yayayaya <rhossain30@stuy.edu>
2022-11-30 01:06:52 -0500

    show user and working path

--- main.c
+++ main.c
@@ -2,15 +2,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
 
 int main()
 {
-    printf("-> ");
-    char *line = malloc(256);
-    while (fgets(line, 256, stdin)) {
-        line[strlen(line) - 1] = '\0';
-        shellexec(line);
-        printf("-> ");
-    }
-
+	char currentDir[100];
+	printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+	char *line = malloc(256);
+	while (fgets(line, 256, stdin)) {
+        	line[strlen(line) - 1] = '\0';
+        	shellexec(line);
+		printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+    	}
 }
