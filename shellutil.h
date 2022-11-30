0e9e05ff41f78e1eb127c380e938bd0fbcb5b89f
Sloan Ireland <sloanireland1@gmail.com>
2022-11-30 11:54:24 -0500

    tryun to impletel semicolon seperation

--- shellutil.h
+++ shellutil.h
@@ -21,4 +21,20 @@ char** parse_args(char *line);
  */
 char *stripcommand(char *cmd);
 
+
+
+/**
+ * check if the provided string contains the provided substring
+ * 
+ * @arg line - the string to check
+ * @arg sub - the substring to check for
+ * @returns 1 if the string contains the substring, 0 otherwise
+ */
+int substring(char * line, char * sub);
+
+/**
+ * break line into individual commands and exec each command
+ * @arg cmd - the string containing the command
+*/
+void *semiColon(char * cmd);
 #endif
