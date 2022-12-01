1d3aed9c017d868a4813f2a8a3b03f477590765f
YAYAYAYAY <rhossain30@stuy.edu>
2022-12-01 10:33:49 -0500

    start sighandler

--- main.c
+++ main.c
@@ -15,20 +15,20 @@
  }
 int main() {
 	signal(SIGINT, sighandler);
-	char currentDir[100];
-	printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+	char currentDir[LINE_BUFFER_SIZE];
+	printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,LINE_BUFFER_SIZE));
 	char *line = malloc(LINE_BUFFER_SIZE);
 	while (fgets(line, LINE_BUFFER_SIZE, stdin)) {
         if (substring(line, " ; ") == 1 || substring(line, "; ") == 1) {
             //printf("Semicolon detected\n");
             semiColon(line);
-            printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+            printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,LINE_BUFFER_SIZE));
         }
         else {
             line[strlen(line) - 1] = '\0';
         	stripcommand(line);
 		    shellexec(line);
-		    printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+		    printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,LINE_BUFFER_SIZE));
         }
         	
     }
