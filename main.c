77ca9eac6c0bbd9e021b5378bfe767b7a1fc3c10
yayayaya <rhossain30@stuy.edu>
2022-12-01 19:35:42 -0500

    fixed signal intercept douple printing

---
+++ main.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <signal.h>
+
+#include "shell.h"
+#include "shellutil.h"
+#include "redirect.h"
+
+#define LINE_BUFFER_SIZE 256
+char currentDir[100];
+static void sigmain(int signo){
+	if (signo == SIGINT){
+		printf("\n\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+		fflush(stdout);
+	}
+}
+static void sigchd(int signo){
+	if (signo==SIGINT){
+		printf("\n");
+		fflush(stdout);
+
+	}
+}
+int main() {
+	signal(SIGINT,sigmain);
+	printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+	char *line = malloc(LINE_BUFFER_SIZE);
+	while (fgets(line, LINE_BUFFER_SIZE, stdin)) {
+        if (strstr(line, " ; ") || strstr(line, "; ") || strstr(line, " ;")) {
+            //printf("Semicolon detected\n");
+            line[strlen(line) - 1] = '\0';
+            semiColon(line);
+            printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+        }
+        else {
+        	line[strlen(line) - 1] = '\0';
+		stripcommand(line);
+        	signal(SIGINT,sigchd);
+		shellexec(line);
+		signal(SIGINT,sigmain);
+		printf("\033[1m%s\033[0m %s -> ", getlogin(), getcwd(currentDir,100));
+        }
+    }  
+}   
