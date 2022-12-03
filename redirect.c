f2344f2c85aed85b187e8a3af3f5693945034ef5
Jeffrey Jiang <jjiang30@stuy.edu>
2022-12-03 05:32:41 -0500

    began working on redirection + completed parsing

--- redirect.c
+++ redirect.c
@@ -2,8 +2,72 @@
 
 #include <fcntl.h>
 #include <unistd.h>
+#include <string.h>
+#include <stdlib.h>
+#include <stdio.h>
 
-#include "shellutil.h"
+#include "shell.h"
+
+void process_cmd_cond(struct cmd_cond *cond, char *command)
+{
+    cond->flag = 0;
+
+    int tok_len = 1;
+    size_t command_len = strlen(command);
+    for (int i = 0; command[i]; ++i, tok_len += command[i] == ' ') {} // counts number of tokens
+    char **tok_arr = malloc(tok_len * sizeof(char *));
+    for (int i = 0; i < tok_len; ++i)
+    {
+        tok_arr[i] = strsep(&command, " "); // sep to tokens
+    }
+
+    for (int i = 0; i < tok_len; ++i)
+    {
+        if (strcmp(tok_arr[i], "<") == 0 && i + 1 < tok_len)
+        {
+            cond->flag |= cmd_in;
+            strcpy(cond->in, tok_arr[i + 1]);
+            ++i;
+        }
+        else if (strcmp(tok_arr[i], ">") == 0 && i + 1 < tok_len)
+        {
+            cond->flag |= cmd_out;
+            cond->flag &= ~cmd_append; // turn append off
+            strcpy(cond->out, tok_arr[i + 1]);
+            ++i;
+        }
+        else if (strcmp(tok_arr[i], ">>") == 0 && i + 1 < tok_len)
+        {
+            cond->flag |= cmd_out | cmd_append;
+            strcpy(cond->out, tok_arr[i + 1]);
+            ++i;
+        }
+        else
+        {
+            cond->flag |= cmd_has_command;
+            strcat(cond->core_command, tok_arr[i]);
+            strcat(cond->core_command, " "); // add space back
+        }
+    }
+
+    for (int i = 0; i < command_len; ++i) // restore to original
+    {
+        if (tok_arr[0][i] == '\0') // note tok_arr[0] should be the the beginning of the original string
+        {
+            tok_arr[0][i] = ' '; // replace back to space
+        }
+    }
+
+    free(tok_arr);
+}
+
+void print_cmd_cond(struct cmd_cond *cond)
+{
+    printf("core_command: %s\n", cond->flag & cmd_has_command ? cond->core_command : "NULL");
+    printf("in: %s\n", cond->flag & cmd_in ? cond->in : "STDIN");
+    printf("out: %s\n", cond->flag & cmd_out ? cond->out : "STDOUT");
+    printf("append: %s\n", cond->flag & cmd_append ? "True" : "False");
+}
 
 void redir_to(char *exec_command, char *file)
 {
@@ -14,4 +78,6 @@ void redir_to(char *exec_command, char *file)
     shellexec(exec_command);
 
     dup2(backup_stdout, STDERR_FILENO);
+
+
 }
--- redirect.h
+++ redirect.h
@@ -1,6 +1,25 @@
 #ifndef REDIRECT_H
 #define REDIRECT_H
 
+struct cmd_cond
+{
+    char flag; // 0 0 0 0 - [has_command][append_out?][default_out?][default_in?]
+    char in[255];
+    char out[255];
+    char core_command[255];
+};
+
+static const char cmd_in = 1 << 0;
+static const char cmd_out = 1 << 1;
+static const char cmd_append = 1 << 2;
+static const char cmd_has_command = 1 << 3;
+
+void process_cmd_cond(struct cmd_cond *cond, char *command);
+
+void exec_cmd(struct cmd_cond *cond);
+
+void print_cmd_cond(struct cmd_cond *cond);
+
 /**
  * executes the provided command and redirects STDOUT to
  * a provided file, overwriting all previous content. 
