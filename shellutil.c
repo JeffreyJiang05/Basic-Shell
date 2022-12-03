0729d80be7c47bef938c8d62be027a8aba2a53fd
Jeffrey Jiang <jjiang30@stuy.edu>
2022-12-03 05:55:39 -0500

    moved parse_args to shellutil.h

--- shellutil.c
+++ shellutil.c
@@ -1,8 +1,29 @@
 #include "shellutil.h"
+
 #include <stdlib.h>
 #include <string.h>
-#include "shell.h"
 #include <stdio.h>
+
+#include "shell.h"
+
+#define MAX_ARGS 10
+
+char** parse_args(char *line)
+{
+    int i = 0;
+    char **args = (char**) malloc(sizeof(void* ) * MAX_ARGS + 1);
+
+    char *ptr;
+    while (i < MAX_ARGS && (ptr = strsep(&line, " ")))
+    {
+        args[i] = ptr;
+        ++i;
+    }
+
+    args[i] = NULL;
+    return args;
+}
+
 char *stripcommand(char *cmd)
 {
     int j = 0;
