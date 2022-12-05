363ce7a58900cf1bca3f9f65b5d125699023c0d5
YAYAYAYAY <rhossain30@stuy.edu>
2022-12-05 11:00:45 -0500

    added header files

--- alias.c
+++ alias.c
@@ -1,8 +1,12 @@
 #include <stdlib.h>
 #include <stdio.h>
+#include <fcntl.h>
+#include <unistd.h>i
+#include <errno.h>
+#include <string.h>
+#include <sys/stat.h>
 
 void printAliases(){
-
 }
 
 int addAlias(char *cmd){
