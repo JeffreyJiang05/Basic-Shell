6f06a846f4f2b4793a0a7e0757669303bb0173d4
yayayaya <rhossain30@stuy.edu>
2022-12-05 02:12:17 -0500

    Start on aliases

---
+++ alias.c
@@ -0,0 +1,14 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+void printAliases(){
+
+}
+
+int addAlias(char *cmd){
+
+}
+
+int removeAlias(char *name){
+
+}
---
+++ alias.h
@@ -0,0 +1,11 @@
+#ifndef ALIAS_H
+#define ALIAS_H
+
+struct alias
+{
+	char name[100];
+	char cmd[255];
+}
+
+#endif
+
