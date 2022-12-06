5c75d861d6b2c473d06e6c4f9e52ba8b1a7a3777
yayayaya <rhossain30@stuy.edu>
2022-12-05 22:55:43 -0500

    Work on compare function

--- alias.c
+++ alias.c
@@ -8,6 +8,17 @@
 #include <string.h>
 #include <sys/stat.h>
 
+/* Check if an alias already exists in the '.aliases' file
+ * with the same name as the argument provided
+ *
+ * @arg name - the name of the alias to be compared
+ * @return 0 if alias with the same name do not exist
+ * non-zero if other aliases exist
+ */
+static int checkExistingAlias(char *name){
+	return 0;
+}
+
 void printAliasi()
 {
 
